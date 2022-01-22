a6dd982c01c3a1e31c577d60e0675512fa29ff29
eon <noe.navarro1019@gmail.com>
2022-01-22 16:42:15 -0700

    added notes file handler to express route

--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -1,8 +1,15 @@
 const path = require('path');
 const router = require('express').Router();
 
+// home page
 router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../../public/index.html'));
 })
+
+// notes page 
+router.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../public/notes.html'));
+})
+
 
 module.exports = router;
